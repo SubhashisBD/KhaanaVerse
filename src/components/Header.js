05976e77049095f1218b469141b439ad8bf12b2d
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-05-24 08:36:00 +0530

    Check status online

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
 
     const [btnName, setbtnName] = useState("Login");
 
+    const OnlineStatus = useOnlineStatus();
 
     return (
         <div className="header">
@@ -15,7 +17,8 @@ const Header = () => {
             </div>
             <div className="nav-items" >
                 <ul>
-                    <li><Link to="/">Home</Link></li>  
+                    <li>OnlineStatus:{OnlineStatus ? "✅" : "🔴"}</li>
+                    <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li>Cart</li>
