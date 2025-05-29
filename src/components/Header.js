cac12ee667c719bf0020c757731ce4cbec3bb74c
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-05-29 11:40:35 +0530

    Added Function to Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 
 import { LOGO_URL } from "../utils/constants";
-import { useState } from "react";
+import { useState,useContext } from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 
 const Header = () => {
 
     const [btnName, setbtnName] = useState("Login");
 
     const OnlineStatus = useOnlineStatus();
+
+    const {loggedInUser} = useContext(UserContext)
+
+    // Subscribing to the store using a Selector (Basically to read how many items present in the slice of Redux cart store)
+    const cartItems = useSelector((store) => store.cart.items)
 
     return (
         <div className="bg-[#FFFDF4] border-b border-gray-200 shadow-sm p-4 flex justify-between items-center">
@@ -20,11 +27,12 @@ const Header = () => {
                     <li>OnlineStatus:{OnlineStatus ? "✅" : "🔴"}</li>
                     <li className="cursor-pointer"><Link to="/">Home</Link></li>
                     <li className="cursor-pointer"><Link to="/about">About Us</Link></li>
-                    <li  className="cursor-pointer"><Link to="/contact">Contact</Link></li>
-                    <li  className="cursor-pointer">Cart</li>
+                    <li className="cursor-pointer"><Link to="/contact">Contact</Link></li>
+                    <li className="cursor-pointer font-bold"><Link to="/cart">Cart - ({cartItems.length}items)</Link></li>
                     <button className="login border-x-zinc-950" onClick={() => {
                         btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
                     }}>{btnName}</button>
+                    <li className="font-medium">{loggedInUser}</li>
                 </ul>
             </div>
 
