1f94c556f505bee34fa8d132fe4a737bdf8988d8
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-05-29 11:44:59 +0530

    Added Swiggy Main API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lo
 export const LOGO_URL="https://static.vecteezy.com/system/resources/previews/023/080/770/non_2x/healthy-food-logo-concept-logo-with-the-symbol-of-a-spoon-fork-and-leaf-can-be-for-restaurants-healthy-food-products-website-logos-for-food-consultants-vector.jpg";
 
 export const MENU_API ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=";
+
+export const SWIGGY_MAIN = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5743545&lng=88.3628734&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
