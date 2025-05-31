7a33942aae0b6f78ec79ece8d9e957734e180c57
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-05-31 23:11:56 +0530

    Test cases for check

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,14 @@
 const Contact = () => {
     return (
-        <h1>This is a Contact Page</h1>
+        <div>
+        <h1 className="text-2xl font-bold">This is a Contact Page</h1>
+        <form>
+            <input type="text" className="border border-black p-2 m-2" placeholder="Name"/>
+            <input type="text" className="border border-black p-2 m-2"placeholder="Message"/>
+            <button className="bg-gray-400 text-black rounded-lg p-2">Submit</button>
+        </form>
+        
+        </div>
     )
 };
 
