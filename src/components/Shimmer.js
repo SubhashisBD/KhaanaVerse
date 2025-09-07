e381693d4006d74c921ccf1de5204496c58bc30f
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-09-07 23:05:55 +0530

    Added shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,19 +1,24 @@
-const Shimmer =()=>{
-    return(
-        <div className ="shimmer-container">
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
-            <div className ="shimmer-card"></div>
+const Shimmer = () => {
+    return (
+        <div className="flex flex-wrap mt-11 ml-11 gap-x-6 gap-y-6">
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
+            <div className="bg-gray-200 rounded-lg shadow-lg h-80 w-56"></div>
         </div>
     );
 };
